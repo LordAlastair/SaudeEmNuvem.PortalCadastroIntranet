bcbed6ee949e30051bbfc0be1f9300140fe7f225
Harlan Gomes <harlangomesnascimento@outlook.com>
2017-10-07 14:12:57 -0300

    Paciente componente alterar cadastro iniciado

---
+++ src/app/portal-cadastro-intranet/paciente/paciente-alterar-cadastro/paciente-alterar-cadastro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-paciente-alterar-cadastro',
+  templateUrl: './paciente-alterar-cadastro.component.html',
+  styleUrls: ['./paciente-alterar-cadastro.component.scss'],
+})
+export class PacienteAlterarCadastroComponent {
+}
