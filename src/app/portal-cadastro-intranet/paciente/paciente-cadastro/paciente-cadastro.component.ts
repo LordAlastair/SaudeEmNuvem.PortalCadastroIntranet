ed1a18d2c182961a3ed461afb9b3f6499d124598
Harlan Gomes <harlangomesnascimento@outlook.com>
2017-10-07 14:23:19 -0300

    Revert "Paciente componente cadastro iniciado"
    This reverts commit a256094cba87ade7cd6837228da4d912c82f9800.

--- src/app/portal-cadastro-intranet/paciente/paciente-cadastro/paciente-cadastro.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-paciente-cadastro',
-  templateUrl: './paciente-cadastro.component.html',
-  styleUrls: ['./paciente-cadastro.component.scss'],
-})
-export class PacienteCadastroComponent {
-}
