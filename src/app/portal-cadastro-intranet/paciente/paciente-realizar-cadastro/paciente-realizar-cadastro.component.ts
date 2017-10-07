aa6e0150ba6c778bd2184c723c754c649c336bd0
Harlan Gomes <harlangomesnascimento@outlook.com>
2017-10-07 14:31:03 -0300

    Paciente componente realizar cadastro iniciado

---
+++ src/app/portal-cadastro-intranet/paciente/paciente-realizar-cadastro/paciente-realizar-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'undefined-paciente-realizar-cadastro',
+  templateUrl: './paciente-realizar-cadastro.component.html',
+  styleUrls: ['./paciente-realizar-cadastro.component.scss']
+})
+export class PacienteRealizarCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
