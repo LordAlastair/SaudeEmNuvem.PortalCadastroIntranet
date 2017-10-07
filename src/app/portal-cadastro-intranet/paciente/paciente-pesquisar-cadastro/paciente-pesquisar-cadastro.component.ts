13acb7b06f184f629b0183ad20982bc823bae9b1
Harlan Gomes <harlangomesnascimento@outlook.com>
2017-10-07 14:18:54 -0300

    paciente componente pesquisar iniciado

---
+++ src/app/portal-cadastro-intranet/paciente/paciente-pesquisar-cadastro/paciente-pesquisar-cadastro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-paciente-pesquisar-cadastro',
+  templateUrl: './paciente-pesquisar-cadastro.component.html',
+  styleUrls: ['./paciente-pesquisar-cadastro.component.scss'],
+})
+export class PacientePesquisarCadastroComponent {
+}
