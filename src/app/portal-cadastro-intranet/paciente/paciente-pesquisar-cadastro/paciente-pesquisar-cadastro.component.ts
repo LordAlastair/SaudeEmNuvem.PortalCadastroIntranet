140ace62ebd2b73167a6bc92ee59c67f289f7b33
Harlan Gomes <harlangomesnascimento@outlook.com>
2017-10-07 14:20:21 -0300

    Revert "paciente componente pesquisar iniciado"
    This reverts commit 13acb7b06f184f629b0183ad20982bc823bae9b1.

--- src/app/portal-cadastro-intranet/paciente/paciente-pesquisar-cadastro/paciente-pesquisar-cadastro.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-paciente-pesquisar-cadastro',
-  templateUrl: './paciente-pesquisar-cadastro.component.html',
-  styleUrls: ['./paciente-pesquisar-cadastro.component.scss'],
-})
-export class PacientePesquisarCadastroComponent {
-}
