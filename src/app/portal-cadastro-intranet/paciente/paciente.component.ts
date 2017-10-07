b36972d74a7fd1e4fd62659fb93279fedcf799ef
Harlan Gomes <harlangomesnascimento@outlook.com>
2017-10-07 14:11:48 -0300

    Paciente module iniciado

---
+++ src/app/portal-cadastro-intranet/paciente/paciente.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-paciente',
+  templateUrl: './paciente.component.html',
+  styleUrls: ['./paciente.component.scss'],
+})
+export class PacienteComponent {
+}
