6c3dfaa7df11f12ddcaeffe25af59cdfdfa9e945
Harlan Gomes Nascimento <harlangomesnascimento@outlook.com>
2017-10-25 17:04:18 -0200

    paciente-realizar-cadastro salvar "funcional"

--- src/app/portal-cadastro-intranet/_services/paciente.service.ts
+++ src/app/portal-cadastro-intranet/_services/paciente.service.ts
@@ -1,28 +1,54 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Paciente } from '../_models/paciente';
+
 
 @Injectable()
 export class PacienteService {
-    constructor(private http: Http) { }
+    constructor() { }
+
+    getData(): Promise<any[]> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(this.getAll());
+            }, 2000);
+        });
+    }
 
     getAll() {
-        return this.http.get('/api/paciente');
+        const data = JSON.parse(localStorage.getItem('pacientes') || '[]');
+        return data;
     }
 
-    getById(codigo: number) {
-        return this.http.get('/api/paciente/');
+    create(paciente) {
+        const pacientes: any[] = JSON.parse(localStorage.getItem('pacientes') || '[]');
+        paciente.codigo = this.generateUUID();
+        pacientes.push(paciente);
+        localStorage.setItem('pacientes', JSON.stringify(pacientes));
     }
 
-    create(paciente: Paciente) {
-        return this.http.post('/api/paciente', paciente);
-    }
+    private generateUUID() {
+        let d = new Date().getTime();
+        const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+          const r = (d + Math.random() * 16) % 16 | 0;
+          d = Math.floor(d / 16);
+          return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return uuid;
+      };
 
-    update(paciente: Paciente) {
-        return this.http.put('/api/paciente/' + paciente.codigo, paciente);
-    }
+    // getById(codigo: number) {
+    //     return this.http.get('/api/paciente/');
+    // }
 
-    delete(id: number) {
-        return this.http.delete('/api/paciente/' + id);
-    }
+    // create(paciente: Paciente) {
+    //     return this.http.post('/api/paciente', paciente);
+    // }
+
+    // update(paciente: Paciente) {
+    //     return this.http.put('/api/paciente/' + paciente.codigo, paciente);
+    // }
+
+    // delete(id: number) {
+    //     return this.http.delete('/api/paciente/' + id);
+    // }
 }
--- src/app/portal-cadastro-intranet/paciente/paciente-realizar-cadastro/paciente-realizar-cadastro.component.ts
+++ src/app/portal-cadastro-intranet/paciente/paciente-realizar-cadastro/paciente-realizar-cadastro.component.ts
@@ -2,6 +2,8 @@ import { FormGroup, FormControl, FormArray, NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { PacienteService } from 'app/portal-cadastro-intranet/_services/paciente.service';
 import { Paciente } from '../../_models/paciente';
+import { ToasterModule, ToasterService, ToasterConfig, Toast } from 'angular2-toaster';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ngx-paciente-realizar-cadastro',
@@ -11,9 +13,10 @@ import { Paciente } from '../../_models/paciente';
 export class PacienteRealizarCadastroComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private pacienteService: PacienteService) { }
+  constructor(private pacienteService: PacienteService, private toasterService: ToasterService) { }
   ngOnInit() {
     this.form = new FormGroup({
+      codigo: new FormControl(),
       nome: new FormControl(),
       cpf: new FormControl(),
       sobreNome: new FormControl(),
@@ -22,10 +25,15 @@ export class PacienteRealizarCadastroComponent implements OnInit {
     });
   }
 
+
   register() {
-    let pacientes: any[] = JSON.parse(localStorage.getItem("pacientes") || '[]');
-    console.log(pacientes);
-    pacientes.push(this.form.value);
-    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+    this.pacienteService.create(this.form.value);
+
+    const toast: Toast = {
+      type: 'success',
+      title: 'Here is a Toast Title',
+      body: 'Here is a Toast Body',
+    };
+    this.toasterService.pop(toast);
   }
 }
