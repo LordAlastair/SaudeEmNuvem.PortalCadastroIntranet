269c7c6c6bf67c93f31cad1f631a04c4e5591df8
Harlan Gomes Nascimento <harlangomesnascimento@outlook.com>
2017-11-01 14:08:59 -0200

    CNS validator fornecido pelo plugin

--- src/app/portal-cadastro-intranet/paciente/paciente-realizar-cadastro/paciente-realizar-cadastro.component.ts
+++ src/app/portal-cadastro-intranet/paciente/paciente-realizar-cadastro/paciente-realizar-cadastro.component.ts
@@ -1,8 +1,8 @@
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
-import { Paciente } from '../../_models/paciente';
-import { ToasterService, Toast } from 'angular2-toaster';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Toast, ToasterService } from 'angular2-toaster';
+
 import { PacienteService } from '../../_services/paciente.service';
 
 @Component({
@@ -49,8 +49,6 @@ export class PacienteRealizarCadastroComponent implements OnInit {
         body: this.form.value.nome + ' ' + this.form.value.apelido + ' ' + 'cadastrado com sucesso',
       };
       this.toasterService.pop(toast);
-
-      // this.router.navigate(['/portal-cadastro-intranet/atendimento/cadastro']);
       this.router.navigate(['/portal-cadastro-intranet/atendimento/cadastro', { codigo: codigo }]);
     } else {
       const toast: Toast = {
@@ -64,67 +62,5 @@ export class PacienteRealizarCadastroComponent implements OnInit {
 
   cancelar() {
     this.router.navigate(['/portal-cadastro-intranet/dashboard']);
-  }
-
-  // Validação CNS
-  private validaCNS(vlrCNS: String) {
-    if (vlrCNS === null || vlrCNS === '') {
-      return false;
-    }
-
-    vlrCNS = vlrCNS.replace(/\s+/g, '');
-
-    if ((vlrCNS.length) !== 15) {
-      return false;
-    }
-
-    let soma: number;
-    let resto: number;
-    let dv: number;
-    let pis: String;
-    let resultado: String;
-
-    pis = vlrCNS.substring(0, 11);
-    soma = (((Number(pis.substring(0, 1))) * 15) +
-      ((Number(pis.substring(1, 2))) * 14) +
-      ((Number(pis.substring(2, 3))) * 13) +
-      ((Number(pis.substring(3, 4))) * 12) +
-      ((Number(pis.substring(4, 5))) * 11) +
-      ((Number(pis.substring(5, 6))) * 10) +
-      ((Number(pis.substring(6, 7))) * 9) +
-      ((Number(pis.substring(7, 8))) * 8) +
-      ((Number(pis.substring(8, 9))) * 7) +
-      ((Number(pis.substring(9, 10))) * 6) +
-      ((Number(pis.substring(10, 11))) * 5));
-
-    resto = soma % 11;
-    dv = 11 - resto;
-    if (dv === 11) {
-      dv = 0;
-    }
-
-    if (dv === 10) {
-      soma = (((Number(pis.substring(0, 1))) * 15) +
-        ((Number(pis.substring(1, 2))) * 14) +
-        ((Number(pis.substring(2, 3))) * 13) +
-        ((Number(pis.substring(3, 4))) * 12) +
-        ((Number(pis.substring(4, 5))) * 11) +
-        ((Number(pis.substring(5, 6))) * 10) +
-        ((Number(pis.substring(6, 7))) * 9) +
-        ((Number(pis.substring(7, 8))) * 8) +
-        ((Number(pis.substring(8, 9))) * 7) +
-        ((Number(pis.substring(9, 10))) * 6) +
-        ((Number(pis.substring(10, 11))) * 5) + 2);
-      resto = soma % 11;
-      dv = 11 - resto;
-      resultado = pis + '001' + String(dv);
-    } else {
-      resultado = pis + '000' + String(dv);
-    }
-    if (vlrCNS !== resultado) {
-      return false;
-    } else {
-      return true;
-    }
   }
 }
