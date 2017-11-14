363ef3a1aab7b1d75979d4ae09009a04a01c15d2
Harlan Gomes Nascimento <harlangomesnascimento@outlook.com>
2017-11-14 17:43:06 -0200

    Removidos os menus->submenu, deixando apenas os botões diretamente

--- src/app/portal-cadastro-intranet/portal-cadastro-intranet-menu.ts
+++ src/app/portal-cadastro-intranet/portal-cadastro-intranet-menu.ts
@@ -8,43 +8,49 @@ export const MENU_ITEMS_PORTAL: NbMenuItem[] = [
     home: true,
   }, {
     title: 'Atendimento',
-    icon: 'ion-person',
-    children: [
-      // {
-      //   title: 'Protocolos desta Sessão',
-      //   link: '/portal-cadastro-intranet/atendimento/visualizarSessao',
-      //   icon: 'ion-person-add',
-      // },
-      {
-        title: 'Pesquisar Protocolo',
-        link: '/portal-cadastro-intranet/atendimento/pesquisar',
-        icon: 'ion-edit',
-      },
-    ],
+    icon: 'ion-person-stalker',
+    link: '/portal-cadastro-intranet/atendimento/pesquisar',
+    // children: [
+    //   // {
+    //   //   title: 'Protocolos desta Sessão',
+    //   //   link: '/portal-cadastro-intranet/atendimento/visualizarSessao',
+    //   //   icon: 'ion-person-add',
+    //   // },
+    //   {
+    //     title: 'Pesquisar Protocolo',
+    //     link: '/portal-cadastro-intranet/atendimento/pesquisar',
+    //     icon: 'ion-edit',
+    //   },
+    // ],
   }, {
-    title: 'Pacientes',
-    icon: 'ion-person',
-    children: [
-      {
-        title: 'Novo cadastro',
-        link: '/portal-cadastro-intranet/paciente/cadastrar',
-        icon: 'ion-person-add',
-      },
-      {
-        title: 'Alterar dados do paciente',
-        link: '/portal-cadastro-intranet/paciente/alterar',
-        icon: 'ion-edit',
-      },
-      {
-        title: 'Pesquisar paciente',
-        link: '/portal-cadastro-intranet/paciente/pesquisar',
-        icon: 'ion-search',
-      },
-      {
-        title: 'Visualizar dados do paciente',
-        link: '/portal-cadastro-intranet/paciente/visualizar',
-        icon: 'ion-document-text',
-      },
-    ],
+    title: 'Cadastrar Paciente',
+    link: '/portal-cadastro-intranet/paciente/cadastrar',
+    icon: 'ion-person-add',
+    // children: [
+    //   {
+    //     title: 'Novo cadastro',
+    //     link: '/portal-cadastro-intranet/paciente/cadastrar',
+    //     icon: 'ion-person-add',
+    //   },
+    // {
+    //   title: 'Alterar dados do paciente',
+    //   link: '/portal-cadastro-intranet/paciente/alterar',
+    //   icon: 'ion-edit',
+    // },
+    //   {
+    //     title: 'Pesquisar paciente',
+    //     link: '/portal-cadastro-intranet/paciente/pesquisar',
+    //     icon: 'ion-search',
+    //   },
+    //   // {
+    //   //   title: 'Visualizar dados do paciente',
+    //   //   link: '/portal-cadastro-intranet/paciente/visualizar',
+    //   //   icon: 'ion-document-text',
+    //   // },
+    // ],
+  }, {
+    title: 'Pesquisar paciente',
+    link: '/portal-cadastro-intranet/paciente/pesquisar',
+    icon: 'ion-search',
   },
 ];
